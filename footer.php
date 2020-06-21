50018a8de3a24a2d00ab102c6e1dccdefe84975b
patrickcharda <patrick.chardavoine@gmail.com>
2020-06-22 00:34:37 +0200

    added logos

---
+++ footer.php
@@ -0,0 +1,6 @@
+<hr />
+<footer>
+<a href="#">Mentions légales</a>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./gbafstyle.css">
+	<link rel="icon" href="./logos/favicon.ico" />
+	<title>GBAF</title>
+</head>
+<body>
+<header>
+	<a href="./index.php"><img src="./logos/logo75x100.png" alt="logo GBAF" /></a>
+</header>
+<hr />
